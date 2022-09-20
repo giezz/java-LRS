50df1d749b495fea93fe2b163f14b162614c1c67
mrnek <mrnekit2002@gmail.com>
2022-09-20 13:18:51 +0300

    laba 12 and lab 14-16 done

--- lab12/src/main/java/giezz/Main.java
+++ lab12/src/main/java/giezz/Main.java
@@ -1,10 +1,13 @@
 package giezz;
 
 import giezz.model.Item;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.OptimisticLockException;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,10 +17,28 @@ import static giezz.HibernateUtil.sessionFactory;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        optimisticWrite();
+        long endOptimisticWrite = System.currentTimeMillis() - startTime;
+        startTime = System.currentTimeMillis();
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction()
+//        Query query = session.createQuery("UPDATE Item SET val = 0");
+//        Query query2 = session.createQuery("UPDATE Item SET version = 0");
+//        query.executeUpdate();
+//        query2.executeUpdate();
+//        session.close();
+        pessimisticWrite();
+        long endPessimisticWrite = System.currentTimeMillis() - startTime;
+        System.out.println("optimistic time " + endOptimisticWrite);
+        System.out.println("pess time " + endPessimisticWrite);
+
+
+        sessionFactory.close();
+    }
+
+    public static void pessimisticWrite() throws InterruptedException {
         int threadAmount = 8;
-        AtomicInteger updateCount = new AtomicInteger();
-        AtomicInteger rollbackCount = new AtomicInteger();
-        CountDownLatch countDownLatch = new CountDownLatch(8);
         Thread[] threads = new Thread[threadAmount];
 
         for (int i = 0; i < threads.length; i++) {
@@ -26,25 +47,28 @@ public class Main {
                 Session session = sessionFactory.openSession();
                 for (int j = 0; j < 20000; j++) {
                     isUpdated = false;
-                    while (!isUpdated){
+                    while (!isUpdated) {
                         try {
                             session.beginTransaction();
-                            Item item = session.get(Item.class, (long) (Math.random() * 40 + 1));
+                            Item item = session
+                                    .createQuery("from Item item where id = :id ", Item.class)
+                                    .setParameter("id", (long) (Math.random() * 40 + 1))
+                                    .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+                                    .getSingleResult();
+//                                    .get(Item.class, (long) (Math.random() * 40 + 1));
                             item.setVal(item.getVal() + 1);
                             session.persist(item);
                             session.getTransaction().commit();
                             System.out.println(Thread.currentThread().getName() + " commited");
-                            updateCount.getAndIncrement();
                             isUpdated = true;
                         } catch (HibernateException | OptimisticLockException e) {
                             session.getTransaction().rollback();
                             System.out.println(Thread.currentThread().getName() + " rollback");
                             System.err.println("in " + Thread.currentThread().getName() + " " + e.getMessage());
-                            rollbackCount.getAndIncrement();
                         }
                     }
                     try {
-                        Thread.sleep(5);
+                        Thread.sleep(1);
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -59,10 +83,50 @@ public class Main {
         for (Thread thread : threads) {
             thread.join();
         }
-        sessionFactory.close();
-        System.out.println("update count" + updateCount);
-        System.out.println("rollback count" + rollbackCount);
     }
 
+    public static void optimisticWrite() throws InterruptedException {
+        int threadAmount = 8;
+        Thread[] threads = new Thread[threadAmount];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                boolean isUpdated;
+                Session session = sessionFactory.openSession();
+                for (int j = 0; j < 20000; j++) {
+                    isUpdated = false;
+                    while (!isUpdated) {
+                        try {
+                            session.beginTransaction();
+                            Item item = session
+                                    .get(Item.class, (long) (Math.random() * 40 + 1));
+                            item.setVal(item.getVal() + 1);
+                            session.persist(item);
+                            session.getTransaction().commit();
+                            System.out.println(Thread.currentThread().getName() + " commited");
+                            isUpdated = true;
+                        } catch (HibernateException | OptimisticLockException e) {
+                            session.getTransaction().rollback();
+                            System.out.println(Thread.currentThread().getName() + " rollback");
+                            System.err.println("in " + Thread.currentThread().getName() + " " + e.getMessage());
+                        }
+                    }
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                session.close();
+            }, "Thread " + i);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
 
 }
