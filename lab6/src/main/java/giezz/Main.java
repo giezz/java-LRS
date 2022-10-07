506818acef6d2f3747d2b4811eb9b97e2cd7e0db
giezz <giezz@vk.com>
2022-10-07 13:04:33 +0300

    laba 6 post "production" changes

--- lab6/src/main/java/giezz/Main.java
+++ lab6/src/main/java/giezz/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static final int SIZE = 60_000_000;
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Введите число потоков до 6");
+        System.out.println("Введите число потоков");
         Scanner scanner = new Scanner(System.in);
         int threadsAmount = scanner.nextInt();
         float[] floats = calculation(threadsAmount);
@@ -16,6 +16,8 @@ public class Main {
     }
 
     public static float[] calculation(int threadsAmount) throws InterruptedException {
+        if (SIZE % threadsAmount != 0) threadsAmount++;
+
         float[][] array = new float[threadsAmount][SIZE / threadsAmount];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
@@ -27,11 +29,12 @@ public class Main {
         Thread[] threads = new Thread[threadsAmount];
         for (int i = 0; i < threads.length; i++) {
             int finalI = i;
+            int finalThreadsAmount = threadsAmount;
             threads[i] = new Thread(() -> {
                 for (int j = 0; j < array[0].length; j++) {
-                    float delta = 0.2f + (finalI * (SIZE / threadsAmount) + j) / 5;
+                    float delta = 0.2f + (finalI * (SIZE / finalThreadsAmount) + j) / 5;
                     array[finalI][j] =
-                            (float) (array[finalI][j] * Math.sin(delta) * Math.cos(delta) * Math.cos(0.4f + (finalI * (SIZE / threadsAmount) + j) / 2));
+                            (float) (array[finalI][j] * Math.sin(delta) * Math.cos(delta) * Math.cos(0.4f + (finalI * (SIZE / finalThreadsAmount) + j) / 2));
                 }
             });
         }
