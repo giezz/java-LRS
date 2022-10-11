aa1cdc68dced0bf24a29b80bf9d59374268b770d
giezz <giezz@vk.com>
2022-10-11 17:04:18 +0300

    laba 6 calculate array with any amount of threads

--- lab6/src/main/java/giezz/Main.java
+++ lab6/src/main/java/giezz/Main.java
@@ -1,5 +1,6 @@
 package giezz;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,30 +12,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int threadsAmount = scanner.nextInt();
         float[] floats = calculation(threadsAmount);
-//        System.out.println(floats);
+//        System.out.println(Arrays.toString(floats));
         System.out.println(floats[SIZE - 1]);
     }
 
     public static float[] calculation(int threadsAmount) throws InterruptedException {
-        if (SIZE % threadsAmount != 0) threadsAmount++;
-
-        float[][] array = new float[threadsAmount][SIZE / threadsAmount];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                array[i][j] = 1;
+        float[][] array;
+        if (SIZE % threadsAmount == 0) {
+            array = new float[threadsAmount][SIZE / threadsAmount];
+            for (float[] floats : array) {
+                Arrays.fill(floats, 1);
+            }
+        } else {
+            array = new float[threadsAmount][];
+            for (int i = 0; i < array.length; i++) {
+                if (i < array.length - 1) {
+                    array[i] = new float[SIZE / threadsAmount];
+                } else {
+                    array[i] = new float[SIZE / threadsAmount + SIZE % threadsAmount];
+                }
+            }
+            for (float[] floats : array) {
+                Arrays.fill(floats, 1);
             }
         }
-
         long a = System.currentTimeMillis();
         Thread[] threads = new Thread[threadsAmount];
         for (int i = 0; i < threads.length; i++) {
             int finalI = i;
-            int finalThreadsAmount = threadsAmount;
             threads[i] = new Thread(() -> {
-                for (int j = 0; j < array[0].length; j++) {
-                    float delta = 0.2f + (finalI * (SIZE / finalThreadsAmount) + j) / 5;
+                for (int j = 0; j < array[finalI].length; j++) {
+                    float delta = 0.2f + (finalI * (SIZE / threadsAmount) + j) / 5;
                     array[finalI][j] =
-                            (float) (array[finalI][j] * Math.sin(delta) * Math.cos(delta) * Math.cos(0.4f + (finalI * (SIZE / finalThreadsAmount) + j) / 2));
+                            (float) (array[finalI][j] * Math.sin(delta) * Math.cos(delta) * Math.cos(0.4f + (finalI * (SIZE / threadsAmount) + j) / 2));
                 }
             });
         }
@@ -48,7 +58,7 @@ public class Main {
 
         float[] finalArray = new float[SIZE];
         for (int i = 0; i < array.length; i++) {
-            System.arraycopy(array[i], 0, finalArray, i * array[i].length, array[i].length);
+            System.arraycopy(array[i], 0, finalArray, i * array[0].length, array[i].length);
         }
         System.out.println(System.currentTimeMillis() - a);
 
